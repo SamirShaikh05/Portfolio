45471a9b38ed3ea00fd38aa83350b95c0227e151
Samir Shaikh <samirshaikh050505@gmail.com>
2026-06-05 21:34:05 +0530

    made the resume text animation better

--- src/components/ui/Resume.jsx
+++ src/components/ui/Resume.jsx
@@ -1,48 +1,52 @@
+const RESUME_URL =
+    "https://drive.google.com/file/d/1qWVbXJI5vL3YGOQ9mMkAEtd3SYr1S-Ys/view?usp=sharing";
+
+function SlideRevealText({ text }) {
+    return (
+        <span className="relative inline-block h-[1em] overflow-hidden leading-none">
+            <span className="sr-only">{text}</span>
+            <span
+                aria-hidden="true"
+                className="block text-[#8f96a3]/65 transition duration-300 ease-out group-hover:-translate-y-full group-hover:opacity-0 group-hover:text-[#f2f2f2]"
+            >
+                {text}
+            </span>
+            <span
+                aria-hidden="true"
+                className="absolute left-0 top-full block text-[#f2f2f2] opacity-0 transition duration-300 ease-out group-hover:-translate-y-full group-hover:opacity-100"
+            >
+                {text}
+            </span>
+        </span>
+    );
+}
+
+function ResumeIcon() {
+    return (
+        <img
+            src="/resume.svg"
+            alt=""
+            aria-hidden="true"
+            className="size-4 opacity-40 invert transition-opacity duration-300 ease-out group-hover:opacity-90 sm:size-4.5"
+        />
+    );
+}
+
 function Resume() {
     return (
         <a
-            href="https://drive.google.com/file/d/1qWVbXJI5vL3YGOQ9mMkAEtd3SYr1S-Ys/view?usp=sharing"
+            href={RESUME_URL}
             target="_blank"
             rel="noopener noreferrer"
-            className="fixed bottom-6 left-6 z-9999 group"
+            aria-label="Open resume"
+            className="group fixed bottom-5 right-4 z-9999 inline-flex
+            items-center gap-2.5 text-sm font-semibold uppercase leading-none
+            tracking-[0.44em] opacity-85 transition-opacity duration-300 
+            ease-out hover:opacity-100 focus:outline-none sm:bottom-16 sm:right-20
+            sm:text-base lg:bottom-10 lg:right-15"
         >
-            {/* Text */}
-            <div className="relative h-6 overflow-hidden">
-                <span
-                    className="block text-cyan-400 font-medium
-          transition-transform duration-500
-          group-hover:-translate-y-full"
-                >
-                    RESUME
-                </span>
-
-                <span
-                    className="absolute left-0 top-full text-cyan-300 font-medium
-          transition-transform duration-500
-          group-hover:-translate-y-full"
-                >
-                    RESUME
-                </span>
-            </div>
-
-            {/* Icon */}
-            <div className="relative h-6 w-6 overflow-hidden">
-                <img
-                    src="/resume.svg"
-                    alt="resume"
-                    className="absolute inset-0 h-full w-full
-          transition-transform duration-500
-          group-hover:-translate-y-full"
-                />
-
-                <img
-                    src="/resume.svg"
-                    alt="resume"
-                    className="absolute inset-0 h-full w-full translate-y-full
-          transition-transform duration-500
-          group-hover:translate-y-0"
-                />
-            </div>
+            <SlideRevealText text="RESUME" />
+            <ResumeIcon />
         </a>
     );
 }
